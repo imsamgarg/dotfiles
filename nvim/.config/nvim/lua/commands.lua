3cba6f6f85822ae653897cb0a34a57614ee3cdf1
Sam Garg <smlgarg123@gmail.com>
2024-11-09 10:51:58 +0530

    nvim: (feat) new nvim custom commands added

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -1,13 +1,34 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_user_command("BdeleteAll", function()
-	vim.cmd("bufdo bd")
+  vim.cmd("bufdo bd")
 end, {})
 
--- create alias for 'split' command to 'hs'
+vim.api.nvim_create_user_command("Q", function()
+  vim.cmd("bd")
+end, {})
+
+-- start ngrok
+vim.api.nvim_create_user_command("Ngrok", function(opts)
+  local port = opts[1] or "3000"
+  vim.cmd("terminal ngrok http --domain=wanted-skunk-blindly.ngrok-free.app " .. port)
+end, {})
+
+-- run node in debug mode
+vim.api.nvim_create_user_command("RunNode", function(opts)
+  local command = opts[1] or "start:qa"
+  local port = opts[2] or "3000"
+  vim.cmd("split | terminal npm run " .. command)
+  vim.cmd("vsplit")
+  vim.cmd("terminal ngrok http --domain=wanted-skunk-blindly.ngrok-free.app " .. port)
+end, {})
+
+vim.api.nvim_create_user_command("FlutterGenL10n", function()
+  vim.cmd("terminal flutter gen-l10n")
+end, {})
