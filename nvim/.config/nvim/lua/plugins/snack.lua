a649c28dd6e5797c27bf7b43cf728d5b0732a3ea
Sam Garg <smlgarg123@gmail.com>
2025-02-12 23:37:47 +0530

    nvim: (feat) snacks setup

--- nvim/.config/nvim/lua/plugins/snack.lua
+++ nvim/.config/nvim/lua/plugins/snack.lua
@@ -7,7 +7,93 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     -- bigfile = { enabled = true },
-    -- dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+███████╗ █████╗ ███╗   ███╗███████╗    ███╗   ██╗██╗   ██╗██╗███╗   ███╗
+██╔════╝██╔══██╗████╗ ████║██╔════╝    ████╗  ██║██║   ██║██║████╗ ████║
+███████╗███████║██╔████╔██║███████╗    ██╔██╗ ██║██║   ██║██║██╔████╔██║
+╚════██║██╔══██║██║╚██╔╝██║╚════██║    ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║
+███████║██║  ██║██║ ╚═╝ ██║███████║    ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝    ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+        ]],
+      },
+      sections = {
+        { section = "header" },
+        -- {
+        --   pane = 2,
+        --   section = "terminal",
+        --   cmd = "colorscript -e square",
+        --   height = 5,
+        --   padding = 1,
+        -- },
+        -- { section = "keys", gap = 1, padding = 1 },
+        -- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        {
+          icon = " ",
+          title = "Projects",
+          section = "projects",
+          indent = 2,
+          padding = 1,
+          limit = 15,
+          -- dirs = {
+          --   "~/personal_projects/flutter/",
+          --   "~/personal_projects/node/",
+          --   "~/work_projects/node",
+          --   "~/work_projects/flutter",
+          -- },
+          -- dirs = function()
+          --   local dirs = {}
+          --   local home = os.getenv("HOME")
+          --   local personal_flutter_projects = vim.fs.dir(home .. "/personal_projects/flutter", {})
+          --
+          --   -- personal_flutter_projects is a iterator
+          --   if personal_flutter_projects then
+          --     for name, type in personal_flutter_projects do
+          --       if type == "directory" then
+          --         table.insert(dirs, home .. "/personal_projects/flutter/" .. name)
+          --       end
+          --     end
+          --   end
+          --
+          --   return dirs
+          --
+          --   -- local personal_projects = {
+          --   --   home .. "/personal_projects/flutter/",
+          --   --   home .. "/personal_projects/node/",
+          --   -- }
+          --   -- local work_projects = {
+          --   --   home .. "/work_projects/node",
+          --   --   home .. "/work_projects/flutter",
+          --   -- }
+          --   -- for _, dir in ipairs(personal_projects) do
+          --   --   table.insert(dirs, dir)
+          --   -- end
+          --   -- for _, dir in ipairs(work_projects) do
+          --   --   table.insert(dirs, dir)
+          --   -- end
+          --   -- return dirs
+          -- end,
+        },
+
+        -- {
+        --   pane = 2,
+        --   icon = " ",
+        --   title = "Git Status",
+        --   section = "terminal",
+        --   enabled = function()
+        --     return Snacks.git.get_root() ~= nil
+        --   end,
+        --   cmd = "git status --short --branch --renames",
+        --   height = 5,
+        --   padding = 1,
+        --   ttl = 5 * 60,
+        --   indent = 3,
+        -- },
+        { section = "startup" },
+      },
+    },
     -- indent = { enabled = true },
     -- input = { enabled = true },
     -- notifier = { enabled = true },
@@ -15,7 +101,7 @@ return {
     -- scroll = { enabled = true },
     -- statuscolumn = { enabled = true },
     -- words = { enabled = true },
-    lazygit = { configure = true },
+    lazygit = { configure = true, enabled = true },
   },
   keys = {
     -- {
@@ -25,15 +111,15 @@ return {
     --   end,
     --   desc = "Git Blame Line",
     -- },
+    -- {
+    --   "<leader>gf",
+    --   function()
+    --     Snacks.lazygit.log_file()
+    --   end,
+    --   desc = "Lazygit Current File History",
+    -- },
     {
-      "<leader>gf",
-      function()
-        Snacks.lazygit.log_file()
-      end,
-      desc = "Lazygit Current File History",
-    },
-    {
-      "<leader>gg",
+      "<leader>g",
       function()
         Snacks.lazygit()
       end,
