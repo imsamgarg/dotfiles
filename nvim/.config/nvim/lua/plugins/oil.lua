ff5726bbcd27318eee02a087abb8520da1461533
Sam Garg <smlgarg123@gmail.com>
2025-04-13 00:26:47 +0530

    nvim: (refactor) enabled the vim oil for dir

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,13 @@
--- return {
--- 	"stevearc/oil.nvim",
--- 	opts = {},
--- 	default_file_explorer = false,
--- 	columns = {
--- 		"icon",
--- 		"permissions",
--- 		"size",
--- 		"mtime",
--- 	},
--- 	dependencies = { "nvim-tree/nvim-web-devicons" },
--- }
-return {}
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  default_file_explorer = false,
+  columns = {
+    "icon",
+    "permissions",
+    "size",
+    "mtime",
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
+-- return {}
