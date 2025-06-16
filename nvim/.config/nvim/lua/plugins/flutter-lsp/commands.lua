61340710c50dbda4af715e056444e623edc39403
Sam Garg <smlgarg123@gmail.com>
2025-06-16 23:40:59 +0530

    nvim: (feat) dart commands logic refactored

--- nvim/.config/nvim/lua/plugins/flutter-lsp/commands.lua
+++ nvim/.config/nvim/lua/plugins/flutter-lsp/commands.lua
@@ -21,60 +21,111 @@ function PerformAction(title, kind)
   end)
 end
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfw",
-  "<cmd>lua PerformAction('Wrap with widget...')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfc",
-  "<cmd>lua PerformAction('Wrap with Center')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfC",
-  "<cmd>lua PerformAction('Wrap with Column')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfp",
-  "<cmd>lua PerformAction('Wrap with Padding')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfs",
-  "<cmd>lua PerformAction('Wrap with SizedBox')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfR",
-  "<cmd>lua PerformAction('Wrap with Row')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfb",
-  "<cmd>lua PerformAction('Wrap with Builder')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfe",
-  "<cmd>lua PerformAction('Wrap with Expanded')<CR>",
-  { noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>cfr",
-  "<cmd>lua PerformAction('Remove this widget')<CR>",
-  { noremap = true, silent = true }
-)
+function RegisterUsingTitle(title, key)
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>cf" .. key,
+    "<cmd>lua PerformAction('" .. title .. "')<CR>",
+    { noremap = true, silent = true }
+  )
+end
+
+function RegisterWrapWith(widget, key)
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>cf" .. key,
+    "<cmd>lua PerformAction('Wrap with " .. widget .. "...')<CR>",
+    { noremap = true, silent = true }
+  )
+end
+
+-- Register wrap with widgets
+RegisterWrapWith("Widget", "w")
+RegisterWrapWith("Center", "c")
+RegisterWrapWith("Column", "C")
+RegisterWrapWith("Padding", "p")
+RegisterWrapWith("SizedBox", "s")
+RegisterWrapWith("Row", "R")
+RegisterWrapWith("Builder", "b")
+RegisterWrapWith("Expanded", "e")
+RegisterWrapWith("Flexible", "E")
+RegisterWrapWith("ValueListenableBuilder", "v")
+RegisterUsingTitle("Remove this widget", "r")
+
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfw",
+--   "<cmd>lua PerformAction('Wrap with widget...')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfc",
+--   "<cmd>lua PerformAction('Wrap with Center')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfC",
+--   "<cmd>lua PerformAction('Wrap with Column')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfp",
+--   "<cmd>lua PerformAction('Wrap with Padding')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfs",
+--   "<cmd>lua PerformAction('Wrap with SizedBox')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfR",
+--   "<cmd>lua PerformAction('Wrap with Row')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfb",
+--   "<cmd>lua PerformAction('Wrap with Builder')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfe",
+--   "<cmd>lua PerformAction('Wrap with Expanded')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfE",
+--   "<cmd>lua PerformAction('Wrap with Flexible')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfv",
+--   "<cmd>lua PerformAction('Wrap with ValueListenableBuilder')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfs",
+--   "<cmd>lua PerformAction('Wrap with SizedBox')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader>cfr",
+--   "<cmd>lua PerformAction('Remove this widget')<CR>",
+--   { noremap = true, silent = true }
+-- )
+-- Register "Import library" command
+
 -- TODO: debug this
 vim.api.nvim_set_keymap(
   "n",
