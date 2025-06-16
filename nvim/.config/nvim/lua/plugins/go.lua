2defbed788d6e48621e5a933efc99ec7956d3e4a
Sam Garg <smlgarg123@gmail.com>
2025-06-16 23:41:59 +0530

    nvim: (fix) fixed go lsp

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -5,12 +5,24 @@ return {
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = function()
-    require("go").setup()
+  config = function(lp, opts)
+    require("go").setup(opts)
+    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require("go.format").goimports()
+      end,
+      group = format_sync_grp,
+    })
   end,
+  opts = {
+    lsp_cfg = true,
+  },
   event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  enabled = true,
 }
 
 -- return {}
